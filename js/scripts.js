46800529e07174f968d5ffbf2301f3ed0aa62ae0
Perpetue <muperpety@gmail.com>
2018-12-21 13:45:57 +0200

    add avariable to scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function mytest()
+{
+var question1=document.forms["myform"]["answer1"].value;
+var question2=document.forms["myform"]["answer2"].value;
+var question3=document.forms["myform"]["answer3"].value;
+var question4=document.forms["myform"]["answer4"].value;
+var question5=document.forms["myform"]["answer5"].value;
+var question6=document.forms["myform"]["answer6"].value;
+var question7=document.forms["myform"]["answer7"].value;
+var question8=document.forms["myform"]["answer8"].value;
+var question9=document.forms["myform"]["answer9"].value;
+var question10=document.forms["myform"]["answer10"].value;
+var marks=0;
