41798a401b645b93428ca467aaf394d0b7c457c6
Perpetue <muperpety@gmail.com>
2018-12-21 13:46:52 +0200

    add function to scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,48 @@ var question8=document.forms["myform"]["answer8"].value;
 var question9=document.forms["myform"]["answer9"].value;
 var question10=document.forms["myform"]["answer10"].value;
 var marks=0;
+if(question1==" C - All of the above")
+{
+  marks=marks+2;
+  }
+if(question2==" A - window.print();")
+{
+  marks=marks+2;
+  }
+if(question3=="C - forEach()")
+{
+  marks=marks+2;
+}
+if(question4==" A - toUpperCase()")
+{
+  marks=marks+2;
+}
+if(question5=="C - concat()")
+{
+  marks=marks+2;
+}
+if(question6=="B - split()")
+{
+  marks=marks+2;
+  }
+if(questiion7==" B - toUpperCase()")
+{
+  marks=marks+2;
+  }
+if(question8=="A - fixed()")
+{
+  marks=marks+2;
+}
+if(question9==" A - map()")
+{
+  marks=marks+2;
+}
+if(question10=="A - toSource()")
+{
+  marks=marks+2;
+}
+alert("your score is:"+marks);
+}
+
+
+
